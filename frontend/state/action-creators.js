95c98a7ed87b8412989b9c99669cefa473d78f7d
Paul Morrow <pmorow93@gmail.com>
2022-05-20 12:05:53 -0500

    Imported Axios

--- frontend/state/action-creators.js
+++ frontend/state/action-creators.js
@@ -1,25 +1,35 @@
+import axios from "axios";
+
 // ❗ You don't need to add extra action creators to achieve MVP
-export function moveClockwise() { }
+export function moveClockwise() {}
 
-export function moveCounterClockwise() { }
+export function moveCounterClockwise() {}
 
-export function selectAnswer() { }
+export function selectAnswer() {}
 
-export function setMessage() { }
+export function setMessage() {}
 
-export function setQuiz() { }
+export function setQuiz() {}
 
-export function inputChange() { }
+export function inputChange() {}
 
-export function resetForm() { }
+export function resetForm() {}
 
 // ❗ Async action creators
 export function fetchQuiz() {
   return function (dispatch) {
+    axios
+      .get("http://localhost:9000/api/quiz/next")
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
     // First, dispatch an action to reset the quiz state (so the "Loading next quiz..." message can display)
     // On successful GET:
     // - Dispatch an action to send the obtained quiz to its state
-  }
+  };
 }
 export function postAnswer() {
   return function (dispatch) {
@@ -27,13 +37,13 @@ export function postAnswer() {
     // - Dispatch an action to reset the selected answer state
     // - Dispatch an action to set the server message to state
     // - Dispatch the fetching of the next quiz
-  }
+  };
 }
 export function postQuiz() {
   return function (dispatch) {
     // On successful POST:
     // - Dispatch the correct message to the the appropriate state
     // - Dispatch the resetting of the form
-  }
+  };
 }
 // ❗ On promise rejections, use log statements or breakpoints, and put an appropriate error message in state
